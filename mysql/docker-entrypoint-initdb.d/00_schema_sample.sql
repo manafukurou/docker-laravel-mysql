357d9eb1b495e8e04e36799a6c3cb5a4cdd01706
Tatsuya Koyama <tatsuyakoyama@TatsuyanoMacBook-Pro.local>
2021-01-01 13:31:59 +0900

    init

---
+++ mysql/docker-entrypoint-initdb.d/00_schema_sample.sql
@@ -0,0 +1,25 @@
+create database `sample_database`;
+
+grant all on sample_database.* to 'admin'@'%' ;
+
+USE `sample_database`;
+
+START TRANSACTION;
+
+CREATE TABLE `users_sample` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `api_token` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`),
+  UNIQUE KEY `idx_api_token` (`api_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+COMMIT;
